38ea3805d0355f96fbb5c7d29f4c1274d71b9414
Beatriz Matias <162923385+BeaMatiass@users.noreply.github.com>
2026-06-08 15:06:26 -0300

    Add motor_esquerdo.cpp to test left motor

---
+++ motor_esquerdo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Teste do motor esquerdo iniciado." << endl;
+    cout << "Motor esquerdo funcionando." << endl;
+    return 0;
+}
